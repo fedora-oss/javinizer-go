fc3d26a6be25d6d7cf080ce7e9a445c024166c0a
LinuxFedora <hoangtheanhhp@gmail.com>
2026-05-30 16:18:29 +0700

    refactor: decouple database lock detection into build-tagged files for cgo and non-cgo environments

--- internal/database/helpers.go
+++ internal/database/helpers.go
@@ -3,11 +3,9 @@ package database
 import (
 	"errors"
 	"fmt"
-	"strings"
 	"time"
 
 	"github.com/fedora-oss/javinizer-go/internal/models"
-	"github.com/mattn/go-sqlite3"
 	"gorm.io/gorm"
 )
 
@@ -16,14 +14,6 @@ func wrapDBErr(op, entity string, err error) error {
 		return nil
 	}
 	return fmt.Errorf("%s %s: %w", op, entity, err)
-}
-
-func isLocked(err error) bool {
-	var sqliteErr *sqlite3.Error
-	if errors.As(err, &sqliteErr) {
-		return sqliteErr.Code == sqlite3.ErrBusy || sqliteErr.Code == sqlite3.ErrLocked
-	}
-	return err != nil && (strings.Contains(err.Error(), "database is locked") || strings.Contains(err.Error(), "database table is locked"))
 }
 
 const defaultLockRetries = 10
